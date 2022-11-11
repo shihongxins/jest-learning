a18e6d92b9c7161e69ff392ecc8a9d42ef6dee88
shihongxins <shihongxins@163.com>
2022-11-11 17:27:27 +0800

    feat: demo2-test3
    mock 时可以将代理实现独立到单独的测试中

--- src/demo2/searchNames.test.js
+++ src/demo2/searchNames.test.js
@@ -1,27 +1,46 @@
 import searchNames from "./searchNames";
+import { getNames } from "./service"
+
+// mock
+// 修改导入的 service 对象，将其原本的对象及其 getNames 方法，模拟替换;
+// 1. ❗注意：此处与代码位置无关，同样会影响 test 1
+// 2. 如果要避免上述情况，或者说有多出需要不同的 mock 值，可以将 mock 的实现逻辑独立出来，先单独引入原 getNames ，然后在每一个测试处单独实现；
+jest.mock("./service.js", () => ({
+  getNames: jest.fn(),
+}));
 
 // test 1
 test('should return empty result when search keyword not matched', () => {
   // given
   const keyword = "foo";
+  // 单独实现
+  getNames.mockImplementation(() => []);
   // when
   const result = searchNames(keyword);
   // then
   expect(result).toEqual([]);
 });
 
-// mock
-// 修改导入的 service 对象，将其原本的对象及其 getNames 方法，模拟替换，❗注意：此处与代码位置无关，同样会影响 test 1
-jest.mock("./service.js", () => ({
-  getNames: jest.fn(() => ['John', 'Bob', 'Paul', 'George']),
-}));
-
 // test 2
 test('should return target results when search keyword matched', () => {
   // given
   const keyword = "John";
+  // 单独实现
+  getNames.mockImplementation(() => ['John', 'Bob', 'Paul', 'George']);
   // when
   const result = searchNames(keyword);
   // then
   expect(result).toEqual(['John']);
+});
+
+// test 3
+test('should return less than 3 result when search keyword matched', () => {
+  // given
+  const keyword = "a";
+  // 单独实现
+  getNames.mockImplementation(() => ['John', 'Bob', 'Paul', 'George', 'Tom']);
+  // when
+  const result = searchNames(keyword);
+  // then
+  expect(result.length).toBeLessThan(3);
 });
