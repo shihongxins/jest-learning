ca429ea70392270da668ab118003aed2567e41db
shihongxins <shihongxins@163.com>
2022-11-11 16:58:31 +0800

    feat: demo2-test2
    使用 jest.mock 模拟原有接口返回值；

--- src/demo2/searchNames.test.js
+++ src/demo2/searchNames.test.js
@@ -9,3 +9,19 @@ test('should return empty result when search keyword not matched', () => {
   // then
   expect(result).toEqual([]);
 });
+
+// mock
+// 修改导入的 service 对象，将其原本的对象及其 getNames 方法，模拟替换，❗注意：此处与代码位置无关，同样会影响 test 1
+jest.mock("./service.js", () => ({
+  getNames: jest.fn(() => ['John', 'Bob', 'Paul', 'George']),
+}));
+
+// test 2
+test('should return target results when search keyword matched', () => {
+  // given
+  const keyword = "John";
+  // when
+  const result = searchNames(keyword);
+  // then
+  expect(result).toEqual(['John']);
+});
